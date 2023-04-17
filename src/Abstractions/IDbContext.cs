2fccbb91d0a518a50e9ca8a7a8237c010b0c1f25
David G. Moore, Jr <dev@dgmjr.io>
2023-04-17 00:04:30 -0400

    testing decomposition

--- src/Abstractions/IDbContext.cs
+++ src/Abstractions/IDbContext.cs
@@ -14,9 +14,13 @@ namespace Microsoft.EntityFrameworkCore.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 
+[Decomopse]
+public class DbContext2 : DbContext {}
+
 /// <summary>
 /// Interface for a database context.  Implements all public properties and methods of <see cref="DbContext"/>.
 /// </summary>
+
 [GenerateInterfaceAttribute(typeof(DbContext))]
 public partial interface IDbContext
 {
